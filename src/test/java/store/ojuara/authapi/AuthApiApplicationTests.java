d9064a2a68ed15e09fdab666b28afa865cef7d58
brunoabneves <brunoabneves97@gmail.com>
2024-03-23 12:55:03 -0300

    Iniciando projeto

---
+++ src/test/java/store/ojuara/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.ojuara.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
